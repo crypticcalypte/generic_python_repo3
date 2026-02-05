04a6c510b1749aca4ab7581ab2e5b2dca1129f49
crypticcalypte <qg1643@gmail.com>
2026-02-05 13:21:24 -0800

    > added coffee program
    everyone else was making the coffee problem and i wanted to do it too

--- BranchingProblems.py
+++ BranchingProblems.py
@@ -13,17 +13,43 @@
 
 # ----------------------------
 
-total_miles = int(input("Distance in miles:"))
-total_mins = 1
+program = input("Enter 'pizza' to see the pizza problem, or 'coffee' to see the coffee problem. ")
 
-if 0 <= total_miles <= 2:
-    total_mins = 15
+if program == "pizza":
+    total_miles = int(input("Distance in miles: "))
+    total_mins = 0
+
+    if 0 <= total_miles <= 2:
+        total_mins = 15
+    elif 3 <= total_miles <= 5:
+        total_mins = 25
+    elif 6 <= total_miles <= 10:
+        total_mins = 40
+    else:
+        print("Sorry, out of delivery range.")
+
     print(f"Total time = {total_mins}.")
-elif 3 <= total_miles <= 5:
-    total_mins = 25
-    print(f"Total time = {total_mins}.")
-elif 6 <= total_miles <= 10:
-    total_mins = 40
-    print(f"Total time = {total_mins}.")
-else:
-    print("Sorry, out of delivery range.")
+
+
+elif program == "coffee":
+    # ------------ theyre all doing coffee so i wanna do that too ------------
+
+    coffee_size = (input("Enter the size: "))
+    shot_count = input("Is there an extra shot?: ")
+    coffee_price = 0
+
+    if coffee_size == "small":
+        coffee_price = 3
+    elif coffee_size == "medium":
+        coffee_price = 4
+    elif coffee_size == "large":
+        coffee_price = 5
+    else:
+        print("Please choose a different size.")
+
+    if shot_count == "yes":
+        coffee_price = coffee_price + 2
+    else:
+        coffee_price = coffee_price
+
+    print(f"Total price is: ${coffee_price}.")
